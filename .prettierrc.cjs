85fd8936170fe6ae265722498e2bcbde38f1d668
badmen007 <1572226525@qq.com>
2023-06-02 23:41:04 +0800

    feat: 代码风格统一

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true, // 使用单引号
+  semi: false, // 不使用分号
+  tabWidth: 2, // 缩进2个空格
+  trailingComma: 'none', // 不使用尾逗号
+  useTabs: false, // 不使用tab缩进
+  endOfLine: 'auto' // 自动识别换行是LF还是CRLF，默认prettier是let
+}
