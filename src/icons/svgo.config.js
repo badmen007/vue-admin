356b4e5c5e3beea78d643f823a313fb73b56ca8b
badmen007 <1572226525@qq.com>
2023-06-03 17:56:50 +0800

    feat: svg组件

---
+++ src/icons/svgo.config.js
@@ -0,0 +1,11 @@
+export default {
+  plugins: [
+    'preset-default', // 默认插件配置
+    {
+      name: 'removeAttrs',
+      params: {
+        attrs: '(fill|stroke)'
+      }
+    }
+  ]
+}
--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,16 @@ import 'normalize.css/normalize.css'
 // 全局样式
 import '@/styles/index.scss'
 
+// 引入icon插件
+import initSvgIcon from '@/icons/index.ts'
+import 'virtual:svg-icons-register'
+
+
 const app = createApp(App)
 app.use(ElementPlus)
 
+// 注册svg-icon组件
+app.use(initSvgIcon)
 app.use(router)
 app.use(createPinia())
 app.mount('#app')
