f2ac986245c5b9765f5be1af2169089b1724e919
badmen007 <1572226525@qq.com>
2023-06-03 09:43:41 +0800

    feat: 集成pinia

--- src/components/Counter/index.vue
+++ src/components/Counter/index.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
     <button @click="handleClick">计数器</button>
-    <span>{{ counter.count}}</span>
+    <span>{{ counter.count }}</span>
   </div>
 </template>
 
-
 <script setup lang="ts">
-  import { useCounterStore } from '@/stores/counter'
+import { useCounterStore } from '@/stores/counter'
 
-  const counter = useCounterStore()
-  const handleClick = () => {
-    counter.increment()
-  }
+const counter = useCounterStore()
+const handleClick = () => {
+  counter.increment()
+}
 </script>
