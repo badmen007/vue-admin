0f907387f775177628f5bba2309d0e78c4aff51a
badmen007 <1572226525@qq.com>
2023-06-03 09:59:47 +0800

    feat: 集成elementplus

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+
+app.use(router)
+app.use(createPinia())
+app.mount('#app')
+
