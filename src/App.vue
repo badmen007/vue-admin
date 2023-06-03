104c9dc0e4fb52b172ddb40201c302a5ce3a3774
badmen007 <1572226525@qq.com>
2023-06-03 09:43:14 +0800

    feat: 集成pinia

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import Counter from '@/components/Counter/index.vue'
+</script>
 
 <template>
   <router-link to="/home">首页</router-link>
   <router-link to="/about">关于</router-link>
   <router-view></router-view>
+  <Counter />
 </template>
 
 <style scoped></style>
