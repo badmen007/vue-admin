6caabe96e3bfdffb82680db0a7306e6687af2aed
badmen007 <1572226525@qq.com>
2023-06-18 13:48:52 +0800

    feat: sidebar组件基本样式及配置

--- components.d.ts
+++ components.d.ts
@@ -11,9 +11,14 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Counter: typeof import('./src/components/Counter/index.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Sidebar: typeof import('./src/layout/components/Sidebar/index.vue')['default']
+    SidebarItem: typeof import('./src/layout/components/Sidebar/SidebarItem.vue')['default']
     SvgIcon: typeof import('./src/components/SvgIcon/index.vue')['default']
   }
 }
