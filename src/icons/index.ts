47cf4bb27b8c8e596c1d157cafe8ef2ba32a582d
badmen007 <1572226525@qq.com>
2023-08-10 22:17:54 +0800

    重新开始

--- src/icons/index.ts
+++
@@ -1,7 +0,0 @@
-import { App } from 'vue'
-
-import SvgIcon from '@/components/SvgIcon/index.vue' // svg components
-
-export default (app: App) => {
-  app.component('svg-icon', SvgIcon)
-}
--- src/views/documentation/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>Documentation page</h1>
-  </div>
-</template>
