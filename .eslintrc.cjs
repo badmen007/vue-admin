5592678ffa313da13ed658328aad6136f4815460
badmen007 <1572226525@qq.com>
2023-08-11 22:14:31 +0800

    feat: 初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    // 环境，针对哪些环境的语法
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    // 集成了哪些规则，别人写好的规则直接拿来用
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-essential",
+    "@vue/prettier"
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint", "vue"],
+  rules: {
+    // 这里是我们自定义的规则
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+        semi: false,
+        tabWidth: 2,
+        trailingComma: "none",
+        useTabs: false,
+        endOfLine: "auto"
+      }
+    ]
+  }
+}
