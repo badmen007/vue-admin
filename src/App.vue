e5b0c44bcf17ab8d27b75ab971b02ee9a78facdc
badmen007 <1572226525@qq.com>
2023-08-13 15:10:39 +0800

    feat: 导航添加 element 组件 size 切换

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <router-view></router-view>
+  <el-config-provider :size="size" :locale="zhCn">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import zhCn from "element-plus/dist/locale/zh-cn.mjs"
+import { storeToRefs } from "pinia"
+import { useAppStore } from "./stores/app"
+const store = useAppStore()
+const { size } = storeToRefs(store)
+</script>
--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,3 +1,4 @@
+import { Size } from "@/plugins/element"
 import { defineStore } from "pinia"
 export const useAppStore = defineStore(
   "app",
@@ -6,21 +7,26 @@ export const useAppStore = defineStore(
     const state = reactive({
       sidebar: {
         opened: true
-      }
+      },
+      size: "default"
     })
     const sidebar = computed(() => state.sidebar)
+    const size = computed(() => state.size)
 
+    const setSize = (size: Size) => {
+      state.size = size
+    }
     //actions
     const toggleSidebar = () => {
       state.sidebar.opened = !state.sidebar.opened
     }
-    return { state, sidebar, toggleSidebar }
+    return { state, sidebar, toggleSidebar, size, setSize }
   },
   {
     // 持久化
     persist: {
       storage: window.sessionStorage,
-      paths: ["state.sidebar.opened"]
+      paths: ["state.sidebar.opened", "state.size"]
     }
   }
 )
